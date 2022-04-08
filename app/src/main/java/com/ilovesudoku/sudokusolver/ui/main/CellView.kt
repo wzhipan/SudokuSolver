a34e67d6a2a3735b1e6d8b435aaa54d7d0eded3b
Zhipan <wzhipan@gmail.com>
2022-04-08 01:40:14 -0700

    create initial layout

---
+++ app/src/main/java/com/ilovesudoku/sudokusolver/ui/main/CellView.kt
@@ -0,0 +1,27 @@
+package com.ilovesudoku.sudokusolver.ui.main
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class CellView :
+    FrameLayout {
+    constructor(
+        context: Context
+    ) : super(context)
+    constructor(
+        context: Context,
+        attrs: AttributeSet?
+    ) : super(context, attrs)
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int
+    ) : super(context, attrs, defStyleAttr)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = measuredWidth
+        setMeasuredDimension(width, width)
+    }
+}
